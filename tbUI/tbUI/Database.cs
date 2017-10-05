4d56e101b544ca900f2d868234c11eaeca1bddfd
BH210-05\ipd <email@com>
2017-10-05 10:54:16 -0400

    fresh start

---
+++ tbUI/tbUI/Database.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tbUI
+{
+    class Database
+    {   //TODO: ADD CONNECTION STRING
+        static String connString = @"Server=tcp:apoole.database.windows.net,1433;Initial Catalog=tankdb;Persist Security Info=False;User ID=sqladmin;Password=tankDatabase1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        SqlConnection conn;
+
+        //open connection string
+        public void OpenConnection()
+        {
+            conn = new SqlConnection(connString);
+            conn.Open();
+        }
+
+
+        //method to execute queries
+        public void ExecuteQueries(string Query_)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand(Query_, conn);
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                Console.Write("EXCEPTION: " + ex.Message);
+            }
+        }
+        //TODO: INSERT, SELECT STATEMENTS 
+
+            //add username
+        public void AddUser(User u)
+        {
+            try {
+                SqlCommand insertcmd = new SqlCommand("INSERT INTO Users (Username) VALUES (@Username)", conn);
+                insertcmd.Parameters.Add(new SqlParameter("@Username", u.Username));
+                insertcmd.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                Console.Write("EXCEPTION: " + ex.Message);
+            }
+
+        }
+
+        //create method for highscore board. 
+        //SELECT Username, wins, losses, gamesPlayed FROM Users ORDER BY wins DESC
+
+        //method to read data. datareader
+        public SqlDataReader DataReader(string Query_)
+        {
+            SqlCommand cmd = new SqlCommand(Query_, conn);
+            SqlDataReader dr = cmd.ExecuteReader();
+            return dr;
+        }
+    }
+}
---
+++ tbUI/tbUI/LoginPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace tbUI
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Window
+    {
+        Database db = new Database();
+
+        public LoginPage()
+        {
+            db.OpenConnection();
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            User player = new User();
+
+            string playername = tbUserLogin.Text;
+
+            if (Regex.IsMatch(playername, "^[a-zA-Z0-9]+$"))
+            {
+                //  db.ExecuteQueries("SELECT Username FROM Users WHERE Username = @Username");
+                if (playername.Length < 3 || playername.Length > 15)
+                {
+                    MessageBox.Show("Username must be between 3 and 15 characters long.", "Username Entry Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                }
+                else
+                {
+                   player.Username = playername;
+                    db.AddUser(player);                  
+                    MainWindow mainWindow = new MainWindow();
+                    mainWindow.Show();
+                    this.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Username must be in letters and number with no spaces","Username Entry Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+
+            //TODO: if username exists, select user
+            // if username doesn't exist, insert new user
+
+
+        }
+    }
+}
