2769994322b8dc0e40dd7a0cfec54dabdd07aae4
derekdcasey <derek.casey@live.ca>
2017-10-10 14:36:59 -0400

    lobby

---
+++ LobbyTest/prototype1/ActiveUser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prototype1
+{
+   public class ActivePlayer
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public DateTime timestamp { get; set; }
+
+        //private static ActivePlayer instance;
+
+        //private ActivePlayer() { }
+
+        //public static ActivePlayer Instance
+        //{
+        //    get
+        //    {
+        //        if (instance == null)
+        //        {
+        //            instance = new ActivePlayer();
+        //        }
+        //        return instance;
+        //    }
+
+        //}
+
+        public override string ToString()
+        {
+            return string.Format("{0}", username);
+        }
+    }
+}
---
+++ LobbyTest/prototype1/Globals.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prototype1
+{
+   public class Globals
+    {
+        public static Game game { get; set; }
+        public static ActivePlayer player { get; set; }
+ 
+        
+    }
+}
+
+      
