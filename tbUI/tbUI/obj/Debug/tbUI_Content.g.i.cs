5ede90cad217b63740f7cdd36e0ca0dbbe3d8f76
BH210-05\ipd <email@com>
2017-10-10 13:09:42 -0400

    tuesday  end class

--- tbUI/tbUI/obj/Debug/tbUI_Content.g.i.cs
+++ tbUI/tbUI/obj/Debug/tbUI_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("mountain.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("tankright.png")]
 
 
